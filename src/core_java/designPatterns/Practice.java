ae1dd2191c5abacad8b4b530bd057215582e8867
vicky1234567890000 <abhisekdas034@gmail.com>
2024-11-30 22:05:31 +0530

    dummy files

--- src/core_java/designPatterns/Practice.java
+++ src/core_java/designPatterns/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("practice java");
         System.out.println("practice java  - 1");
+        System.out.println("practice java  - 2");
     }
 }
