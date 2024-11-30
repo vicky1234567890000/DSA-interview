67a45466242241cbb606a006b18eaad469ad2fa1
vicky1234567890000 <abhisekdas034@gmail.com>
2024-11-30 22:08:29 +0530

    committing practice.java - 1

--- src/core_java/designPatterns/Practice.java
+++ src/core_java/designPatterns/Practice.java
@@ -3,7 +3,7 @@ package core_java.designPatterns;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("practice java");
-        System.out.println("practice java  - 1");
-        System.out.println("practice java  - 2");
+        System.out.println("practice devops");
+        System.out.println("practice database");
     }
 }
