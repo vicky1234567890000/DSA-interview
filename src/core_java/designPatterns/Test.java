2fe6eed1f28dcaad1db13ea92f8202ef28a73972
vicky1234567890000 <abhisekdas034@gmail.com>
2024-11-30 23:23:15 +0530

    test - 2

--- src/core_java/designPatterns/Test.java
+++ src/core_java/designPatterns/Test.java
@@ -1,2 +1,8 @@
-package core_java.designPatterns;public class Test {
+package core_java.designPatterns;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test - 1");
+        System.out.println("test - 2");
+    }
 }
