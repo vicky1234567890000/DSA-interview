1e6859fe07568b3e917eba91dcf1bf452909a82c
Abhisek Das <40667508+vicky1234567890000@users.noreply.github.com>
2024-11-30 23:28:14 +0530

    Test (#6)
    * new added practice programs
    
    * dummy files
    
    * committing practice.java - 1
    
    * committing practice.java - 2
    
    * committing practice.java - 1
    
    * test - 1
    
    * test - 2
    
    * test - 1
    
    ---------
    
    Co-authored-by: vicky1234567890000 <abhisekdas034@gmail.com>

---
+++ src/core_java/designPatterns/Test.java
@@ -0,0 +1,9 @@
+
+package core_java.designPatterns;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test - 1");
+        System.out.println("test - 2");
+    }
+}
