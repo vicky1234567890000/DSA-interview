730c14260d9deb4a7136a7f2ecc2dfa5e742d00a
vicky1234567890000 <abhisekdas034@gmail.com>
2024-11-30 23:08:13 +0530

    committing practice.java - 2

--- src/core_java/designPatterns/Practice.java
+++ src/core_java/designPatterns/Practice.java
@@ -5,5 +5,6 @@ public class Practice {
         System.out.println("practice java");
         System.out.println("practice devops");
         System.out.println("practice database");
+        System.out.println("practice ML");
     }
 }
